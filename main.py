05ea2f5304a57933484204213dc3486f9264bfa3
onkarmane <onkar373@gmail.com>
2025-01-23 12:43:20 +0530

    "initial"

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # main.py
 # This is the first line change
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Hello, {name}hello!local1"
 
 def farewell(name):
-    return f"Goodbye, {name}hello remote!"
+    return f"Goodbye, {name}hello local1!"
 
 def welcome(name):
-    return f"Welcome, {name}!"
+    return f"Welcome, {name}hello!local1"
 
 if __name__ == "__main__":
     print(greet("World"))
     print(farewell("World"))
     print(welcome("World"))
-# This is the last line change
+
