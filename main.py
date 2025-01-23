70e907d809d7a05b465fb780550bd353b7d51666
onkarmane <onkar373@gmail.com>
2025-01-23 19:02:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # main.py
 # This is the first line change
 def greet(name):
-    return f"Hello, {name}hello"
+    return f"Hello, {name}hello develop"
 
 def farewell(name):
-    return f"Goodbye, {name}hello"
+    return f"Goodbye, {name}hello develop"
 
 def welcome(name):
-    return f"Welcome, {name}hello!"
+    return f"Welcome, {name}hello!develop"
 
 if __name__ == "__main__":
     print(greet("World"))
